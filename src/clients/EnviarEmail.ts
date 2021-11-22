af26a43fd773ef1e0fe9f79ed1356c98a06e7b3e
Joao <joaotst@hotmail.com>
2021-11-22 18:42:27 -0300

    Cria o sistema de envio de e-mail via Sendgrid

---
+++ src/clients/EnviarEmail.ts
@@ -0,0 +1,27 @@
+import * as sgMail from "@sendgrid/mail";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const mensagem = {
+  to: "ajutram.adv@gmail.com",
+  from: "jpsilva.dev@gmail.com",
+  subject: "Testando e-mail com sendgrid",
+  text: "teste de e-mail com sendgrid",
+  html: "<h2> O campo html substitui o texto na tag text</h2>",
+};
+
+export const enviarEmail = void (async () => {
+  try {
+    await sgMail.send(mensagem);
+    console.log("E-mail enviado com sucesso!!!");
+  } catch (error) {
+    console.error(error);
+
+    if (error.response) {
+      console.error(error.response.body);
+    }
+  }
+})();
